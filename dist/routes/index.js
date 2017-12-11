c0d10321150d3429a6f04184599ec0835652053b
sfyr111 <sfyr111@163.com>
2017-12-11 19:06:29 +0800

    fix appid

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -3,18 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const User = require("../controller/user/user");
 const OAuth = require("wechat-oauth");
-if (process.env.NODE_ENV !== 'production') {
-    const APP_ID = 'wx78dfb7976e77c436';
-    const APP_SECRET = '1a55760202297f214c23a5dd9514646e';
-    const wechatClient = new OAuth(APP_ID, APP_SECRET);
-    const redirectUrl = 'http://mpsanqian.free.ngrok.cc';
-}
-else {
-    const APP_ID = 'wx1a679722114b6a84';
-    const APP_SECRET = '41a839d5d19753a8032b86a33c87b8e8';
-    const wechatClient = new OAuth(APP_ID, APP_SECRET);
-    const redirectUrl = 'http://www.ai-union.com';
-}
+const APP_ID = process.env.NODE_ENV !== 'production' ? 'wx78dfb7976e77c436' : 'wx1a679722114b6a84';
+const APP_SECRET = process.env.NODE_ENV !== 'production' ? '1a55760202297f214c23a5dd9514646e' : '41a839d5d19753a8032b86a33c87b8e8';
+const wechatClient = new OAuth(APP_ID, APP_SECRET);
+const redirectUrl = process.env.NODE_ENV !== 'production' ? 'http://mpsanqian.free.ngrok.cc' : 'http://www.ai-union.com';
 const router = express.Router();
 router.route('/')
     .get(function (req, res, next) {

27515d538ad27ae49fefdfbfdf60a87c7364d7d9
sfyr111 <sfyr111@163.com>
2018-03-22 00:31:02 +0800

    fix redis

--- dist/services/redis-service.js
+++ dist/services/redis-service.js
@@ -1,15 +1,15 @@
 "use strict";
 exports.__esModule = true;
-var Redis = require("ioredis");
 // const redis = new Redis({
 //   host: '61.147.124.83',
 //   port: 7000
 // })
-var redis = new Redis.Cluster([
-    { port: 7000, host: '61.147.124.83' },
-    { port: 7001, host: '61.147.124.67' },
-    { port: 7002, host: '61.147.124.73' },
-    { port: 7003, host: '61.147.124.74' },
-    { port: 7004, host: '61.147.124.76' }
-]);
+// const redis = new Redis.Cluster([
+//   { port: 7000, host: '61.147.124.83' },
+//   { port: 7001, host: '61.147.124.67' },
+//   { port: 7002, host: '61.147.124.73' },
+//   { port: 7003, host: '61.147.124.74' },
+//   { port: 7004, host: '61.147.124.76' }
+// ])
+var redis = {};
 exports["default"] = redis;
--- src/services/redis-service.ts
+++ src/services/redis-service.ts
@@ -5,12 +5,14 @@ import * as Redis from 'ioredis'
 //   port: 7000
 // })
 
-const redis = new Redis.Cluster([
-  { port: 7000, host: '61.147.124.83' },
-  { port: 7001, host: '61.147.124.67' },
-  { port: 7002, host: '61.147.124.73' },
-  { port: 7003, host: '61.147.124.74' },
-  { port: 7004, host: '61.147.124.76' }
-])
+// const redis = new Redis.Cluster([
+//   { port: 7000, host: '61.147.124.83' },
+//   { port: 7001, host: '61.147.124.67' },
+//   { port: 7002, host: '61.147.124.73' },
+//   { port: 7003, host: '61.147.124.74' },
+//   { port: 7004, host: '61.147.124.76' }
+// ])
+
+const redis = {}
 
 export default redis
